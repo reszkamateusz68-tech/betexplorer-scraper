e1dd4a159ebf687678a1307841a6a983ebaac5f4
reszkamateusz68-tech <reszka.mateusz68@gmail.com>
2026-06-11 15:45:16 +0200

    Update betexplorer_all.py

--- betexplorer_all.py
+++ betexplorer_all.py
@@ -111,7 +111,14 @@ def split_datetime(value):
 urls = pd.read_excel("ligi.xlsx")["URL"].dropna().tolist()
 
 headers = {
-    "User-Agent": "Mozilla/5.0"
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/137.0.0.0 Safari/537.36"
+    ),
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Cache-Control": "no-cache"
 }
 
 all_data = []
