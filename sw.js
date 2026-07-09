1c270cfcb7d76bcd002f5cb5102c65ad6ac30cdc
reszkamateusz68-tech <reszka.mateusz68@gmail.com>
2026-07-09 08:36:47 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,12 @@
-// Pusty Service Worker, który oszukuje wymogi instalacji PWA w Chrome
 self.addEventListener('install', (e) => {
     self.skipWaiting();
 });
 
+self.addEventListener('activate', (e) => {
+    e.waitUntil(self.clients.claim());
+});
+
 self.addEventListener('fetch', (e) => {
-    // Nie robimy nic, po prostu przepuszczamy ruch
+    // Przepuszcza ruch, spełniając wymóg Chrome dla aplikacji PWA
+    e.respondWith(fetch(e.request).catch(() => new Response('Oczekiwanie na połączenie sieciowe...')));
 });
